bd33385844c3964c057db1e0ec482591b0647958
Zuzuna54 <giorgigiorgobiani@hotmail.com>
2019-10-17 20:00:16 -0700

    modified file structure

--- src/sentiment.js
+++ src/sentiment.js
@@ -1,4 +1,36 @@
 
+var twitter = require('twitter');
+
+
+
+var twitter = new twitter({
+    consumer_key: 'mGH2G3H23QnqEoxep2irFceHZ',
+    consumer_secret: '2Hga9WVHSDfHWQ3JI14UNBZdbtI1Ce6TczCbriI1rNFVA2xk7L',
+    access_token_key: '1184129891748499456-KY8Oj8QdxQigCBvCaRSlyucqXTIS7q',
+    access_token_secret: 'ZyQRmrvGoYDgid3Syxaq18vIGvcB6nJYu3ejlRFWguds8'
+});
+
+const tweetsLoader = (input) => {
+
+    var search = input
+
+    let tweeets = []
+
+    twitter.stream('statuses/filter', { track: search }, function (stream) {
+        stream.on('data', function (tweet) {
+            tweeets.push(tweet.text)
+            // console.log(tweeets)
+            if (tweeets.length === 100) {
+                stream.destroy();
+                return tweeets
+            }
+        });
+        stream.on('error', function (error) {
+        });
+    });
+}
+
+
 async function quickstart(input) {
     // Imports the Google Cloud client library
     const language = require('@google-cloud/language');
@@ -23,4 +55,6 @@ async function quickstart(input) {
     console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
 }
 
+
+console.log(tweetsLoader("trump"))
  
