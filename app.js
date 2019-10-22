bd4d78c68a9a91a2248d1151d30ad0d2ccc028e9
Zuzuna54 <giorgigiorgobiani@hotmail.com>
2019-10-22 07:50:34 -0700

    adding D3

--- app.js
+++ app.js
@@ -2,31 +2,22 @@ const express = require("express");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 var path = require("path");
-
-
-
-
 const app = express();
 var http = require('http').createServer(app)
+var twitter = require('twitter');
 
 
-app.get('/', function (req, res) {
-    res.sendFile(path.resolve(__dirname + '/frontend/index.html'));
+app.get('/tweets', function (req, res) {
+    tweetsLoader("trump");
+    return setTimeout(function(){ res.json(tweeets);}, 500)
 }); 
 
-
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-
-var twitter = require('twitter');
-
-
 
 var twitter = new twitter({
     consumer_key: 'mGH2G3H23QnqEoxep2irFceHZ',
@@ -35,50 +26,53 @@ var twitter = new twitter({
     access_token_secret: 'ZyQRmrvGoYDgid3Syxaq18vIGvcB6nJYu3ejlRFWguds8'
 });
 
-let tweeets = []
+async function quickstart(input) {
+    // Imports the Google Cloud client library
+    const language = require('@google-cloud/language');
+    
+    // Instantiates a client
+    const client = new language.LanguageServiceClient();
+    
+    // The text to analyze
+    const text = input;
+    
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
+    
+    // Detects the sentiment of the text
+    const [result] = await client.analyzeSentiment({ document: document });
+    const sentiment = result.documentSentiment;
+    
+    const output = { text: text, sentimentScore: sentiment.score, sentimentMagnitude: sentiment.magnitude }
+    // console.log(output)
+    return output
+}
+
+let tweeets = [];
 const tweetsLoader = (input, res) => {
-
-    var search = input
-
-
+    var search = input;
+    var output = [];
+    
     twitter.stream('statuses/filter', { track: search }, function (stream) {
         stream.on('data', function (tweet) {
-            tweeets.push(tweet.text)
+            // console.log(tweet.extended_tweet)
+            let text = quickstart(tweet.extended_tweet.full_text)
+            output.push({date: tweet.created_at, user: { screen_name: tweet.user.screen_name, name: tweet.user.name}, text: text });
             // res.send(tweet);
-            console.log(tweet.text)
-            if (tweeets.length >= 100) {
+            console.log(tweeets)
+            tweeets = output;
+            if (tweeets.length >= 10) {
                 stream.destroy();
-                return tweeets
+                output = [];
+                return tweeets;
             }
         });
         stream.on('error', function (error) {
         });
     });
 }
-
-// console.log(tweetsLoader("banana"))
-
-
-async function quickstart(input) {
-    // Imports the Google Cloud client library
-    const language = require('@google-cloud/language');
-
-    // Instantiates a client
-    const client = new language.LanguageServiceClient();
-
-    // The text to analyze
-    const text = input;
-
-    const document = {
-        content: text,
-        type: 'PLAIN_TEXT',
-    };
-
-    // Detects the sentiment of the text
-    const [result] = await client.analyzeSentiment({ document: document });
-    const sentiment = result.documentSentiment;
-
-    console.log(`Text: ${text}`);
-    console.log(`Sentiment score: ${sentiment.score}`);
-    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-}
+console.log(tweetsLoader("trump"))
+// setTimeout( function(){console.log(tweeets)}, 1000)
+console.log(tweeets)
